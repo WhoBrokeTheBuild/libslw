95bd7e6619c6f09aa3313bb2ad6fcd3475010baa
Stephen Lane-Walsh <sdl.slane@gmail.com>
2026-07-15 00:13:58 -0400

    Add fallback views::enumerate()

--- Public/slw/ranges.hpp
+++ Public/slw/ranges.hpp
@@ -16,6 +16,60 @@ namespace views {
     
     using namespace std::views;
 
+    #ifndef __cpp_lib_ranges_enumerate
+
+        template <typename Iterable>
+        class _enumerator
+        {
+        public:
+
+            _enumerator(Iterable iterable)
+                : _index(0)
+                , _iterable(iterable)
+                , _iterator(std::begin(iterable))
+                , _end(std::end(iterable))
+            { }
+
+            inline const _enumerator& begin() const {
+                return *this;
+            }
+
+            inline const _enumerator& end() const {
+                return *this;
+            }
+
+            inline bool operator!=(const _enumerator&) const {
+                return _iterator != _end;
+            }
+
+            inline void operator++() {
+                ++_iterator;
+                ++_index;
+            }
+
+            inline auto operator*() const {
+                return std::pair(_index, *_iterator);
+            }
+
+        private:
+
+            std::size_t _index;
+
+            Iterable _iterable;
+
+            decltype(std::begin(_iterable)) _iterator;
+
+            const decltype(std::end(_iterable)) _end;
+
+        };
+
+        template <typename Iterable>
+        inline auto enumerate(Iterable&& iterable) {
+            return _enumerator<Iterable>(std::forward<Iterable>(iterable));
+        }
+
+    #endif
+
 } // namespace views
 
 } // namespace slw
