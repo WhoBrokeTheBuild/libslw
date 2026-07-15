9735015ca4ac76bc073f09b61fc7ed0219dc8f80
Stephen Lane-Walsh <sdl.slane@gmail.com>
2026-07-15 00:13:38 -0400

    Add chrono and thread passthrough headers

---
+++ Public/slw/chrono.hpp
@@ -0,0 +1,14 @@
+#ifndef SLW_CHRONO_HPP
+#define SLW_CHRONO_HPP
+
+#include <chrono>
+
+namespace slw {
+
+using namespace std::chrono;
+
+using namespace std::chrono_literals;
+
+} // namespace slw
+
+#endif // SLW_CHRONO_HPP
---
+++ Public/slw/thread.hpp
@@ -0,0 +1,18 @@
+#ifndef SLW_THREAD_HPP
+#define SLW_THREAD_HPP
+
+#include <slw/chrono.hpp>
+
+#include <thread>
+
+namespace slw {
+
+using std::thread;
+
+namespace this_thread {
+    using namespace std::this_thread;
+}
+
+} // namespace slw
+
+#endif // SLW_THREAD_HPP
